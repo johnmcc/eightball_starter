d1b0974da4f67e0b2460615c0444e0fad66d6086
John McCollum <johnvmccollum@gmail.com>
2017-02-27 11:51:36 +0000

    Add .iml files

---
+++ app/src/main/java/com/codeclan/example/magiceightball/EightBallActivity.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.magiceightball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EightBallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eight_ball2);
+    }
+}
