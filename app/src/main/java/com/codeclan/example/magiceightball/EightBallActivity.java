2a421036932321080b343addbe057cca3d5c45b5
John McCollum <johnvmccollum@gmail.com>
2017-02-27 14:16:09 +0000

    Added click handler

--- app/src/main/java/com/codeclan/example/magiceightball/EightBallActivity.java
+++ app/src/main/java/com/codeclan/example/magiceightball/EightBallActivity.java
@@ -2,12 +2,29 @@ package com.codeclan.example.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EightBallActivity extends AppCompatActivity {
+
+    EditText questionEditText;
+    Button shakeButton;
+    TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eight_ball2);
+
+        questionEditText = (EditText) findViewById(R.id.question_edittext);
+        shakeButton = (Button) findViewById(R.id.shake_btn);
+        answerText = (TextView) findViewById(R.id.answer_text);
+    }
+
+    public void onShakeButtonClicked(View button){
+
     }
 }
